f96cc8d1aa632a5d9738cb1c9d3472c2b2a605ed
Luis Febro <mr.febro@gmail.com>
2020-04-01 15:56:33 -0400

    testing sessions - navbar and new user database renaming pt2

--- client/src/serviceWorker.js
+++ client/src/serviceWorker.js
@@ -1,7 +1,7 @@
 import showVanillaToast from './components/vanilla-js/toastify/showVanillaToast';
 
 const isToastActivated = true;
-// WARNING: The currentVersion needs to be actually the NEXT ONE because the next cache client receives is not updated.
+// WARNING: The currentVersion needsfds to be actually the NEXT ONE because the next cache client receives is not updated.
 const currentVersion = "2.3";
 
 const isLocalhost = Boolean(
