1c151683a4156223f95b30e6ca6ef6dd99769ef7
Luis Febro <mr.febro@gmail.com>
2020-05-27 19:27:01 -0400

    dash - cli-ao search if there are users for a challenge, add tooltip to instru button, add limitFree mechanism || tests and error fixing pt 3

--- client/src/components/_layout/navbar/Navbar.js
+++ client/src/components/_layout/navbar/Navbar.js
@@ -155,7 +155,7 @@ function Navbar({ history, location }) {
     return (
         <NavWrapper
             className="navbar navbar-expand-sm text-nav-items"
-            style={{backgroundColor: locationNow.includes("/painel-de-controle") ? "var(--themePDark--default)" : "var(--themePDark--" + selfThemePColor + ")" }}
+            style={{backgroundColor: (!isThisApp() || locationNow.includes("/painel-de-controle")) ? "var(--themePDark--default)" : "var(--themePDark--" + selfThemePColor + ")" }}
         >
             {showLogo()}
             {showRoleTitles()}
