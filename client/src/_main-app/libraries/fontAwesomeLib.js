3e69b9b3b013b169fc3382b9923cc95997d1be8d
Luis Febro <mr.febro@gmail.com>
2020-06-14 07:30:33 -0400

    performance optimazation - solving render-blocking and unused css with purgeCSS, craco, webpack (fontawesome, bootstrap optimized) pt6

--- client/src/_main-app/libraries/fontAwesomeLib.js
+++ client/src/_main-app/libraries/fontAwesomeLib.js
@@ -1,143 +1,164 @@
 // copy this to every component which needs icons:
 // USE: import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; <FontAwesomeIcon icon="heart" />
+// Warning: this destructing by line is due to tree-shaking and avoid to import the entire fas icons and thus a bloated bundle.
+// const fasPath = "@fortawesome/free-solid-svg-icons"; // Not possible for now to declare a variable and use it as >> ... from `${fasPath}/faCheck`...
 import { library } from '@fortawesome/fontawesome-svg-core';
-
-import {
-    // app milestone icons
-    faHeart,
-    faStar,
-    faSun,
-    faIceCream,
-    faHamburger,
-    faCut,
-    faCircle,
-    faPaw,
-    faTshirt,
-    faCocktail,
-    faMusic,
-    faRobot,
-    faMedal,
-    faStethoscope,
-    faCar,
-    faDragon,
-    faGrinHearts,
-    faUserTie,
-    faFire,
-    faGamepad,
-    faDumbbell,
-    faAppleAlt,
-    faCartPlus,
-    // end app milestone icons
-    faFlagCheckered,
-    faExclamationCircle, // for errors
-    faInfoCircle, // for warning
-    faCheckCircle, // for success
-    faCheck, // Confirmation btn
-    faPaperPlane, // for send data
-    faLock, // for authentication, password
-    faSave,
-    faKeyboard,
-    faAngleDoubleRight, // generate something,
-    faQuestion,
-    faMobileAlt,
-    faShareAlt,
-    faMinus,
-    faPlus,
-    faTimes, // keypad cancel btn
-    faTimesCircle, // closing btn
-    faArrowLeft,
-    faListOl, // CPF keypad // history
-    faListUl, // score regulation
-    faMoneyBillAlt, // Purchase Value keypad
-    faTrashAlt, // delete btn
-    faMinusCircle, // discount btn
-    faUser,
-    faQuestionCircle, // instruction btn
-    faShoppingCart,
-    faTrophy,
-    faCrown,
-    faPalette,
-    faGem, // milestone icons and reward config
-    faComment,
-    faClock,
-    faFileAlt,
-    faDatabase, // security copies
-    faCogs, // tech support, setting
-    faChartPie,
-    faHome,
-    faAngleRight,
-    faLongArrowAltDown,
-    faImage,
-    faSyncAlt, // change something, update
-    faReply, // back, return
-} from '@fortawesome/free-solid-svg-icons';
+//ICONS n1
+import { faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons/faAngleDoubleRight'; // generate something
+import { faAngleRight } from '@fortawesome/free-solid-svg-icons/faAngleRight';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons/faArrowLeft';
+import { faChartPie } from '@fortawesome/free-solid-svg-icons/faChartPie';
+import { faCheck } from '@fortawesome/free-solid-svg-icons/faCheck'; // Confirmation btn
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons/faCheckCircle'; // for success
+import { faClock } from '@fortawesome/free-solid-svg-icons/faClock';
+import { faCogs } from '@fortawesome/free-solid-svg-icons/faCogs'; // tech support, setting
+import { faComment } from '@fortawesome/free-solid-svg-icons/faComment';
+import { faCrown } from '@fortawesome/free-solid-svg-icons/faCrown';
+import { faDatabase } from '@fortawesome/free-solid-svg-icons/faDatabase'; // security copies
+import { faExclamationCircle } from '@fortawesome/free-solid-svg-icons/faExclamationCircle'; // for errors
+import { faFileAlt } from '@fortawesome/free-solid-svg-icons/faFileAlt';
+import { faFlagCheckered } from '@fortawesome/free-solid-svg-icons/faFlagCheckered';
+import { faGem } from '@fortawesome/free-solid-svg-icons/faGem'; // milestone icons and reward config
+import { faHome } from '@fortawesome/free-solid-svg-icons/faHome';
+import { faImage } from '@fortawesome/free-solid-svg-icons/faImage';
+import { faInfoCircle } from '@fortawesome/free-solid-svg-icons/faInfoCircle'; // for warning
+import { faKeyboard } from '@fortawesome/free-solid-svg-icons/faKeyboard';
+import { faListOl } from '@fortawesome/free-solid-svg-icons/faListOl'; // CPF keypad // history
+import { faListUl } from '@fortawesome/free-solid-svg-icons/faListUl'; // score regulation
+import { faLock } from '@fortawesome/free-solid-svg-icons/faLock'; // for authentication, password
+import { faLongArrowAltDown } from '@fortawesome/free-solid-svg-icons/faLongArrowAltDown';
+import { faMinus } from '@fortawesome/free-solid-svg-icons/faMinus';
+import { faMinusCircle } from '@fortawesome/free-solid-svg-icons/faMinusCircle'; // discount btn
+import { faMobileAlt } from '@fortawesome/free-solid-svg-icons/faMobileAlt';
+import { faMoneyBillAlt } from '@fortawesome/free-solid-svg-icons/faMoneyBillAlt'; // Purchase Value keypad
+import { faPalette } from '@fortawesome/free-solid-svg-icons/faPalette'; // for send data
+import { faPaperPlane } from '@fortawesome/free-solid-svg-icons/faPaperPlane'; // for send data
+import { faPlus } from '@fortawesome/free-solid-svg-icons/faPlus';
+import { faQuestion } from '@fortawesome/free-solid-svg-icons/faQuestion';
+import { faQuestionCircle } from '@fortawesome/free-solid-svg-icons/faQuestionCircle'; // instruction btn
+import { faReply } from '@fortawesome/free-solid-svg-icons/faReply'; // back, return
+import { faSave } from '@fortawesome/free-solid-svg-icons/faSave';
+import { faShareAlt } from '@fortawesome/free-solid-svg-icons/faShareAlt';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons/faShoppingCart';
+import { faSyncAlt } from '@fortawesome/free-solid-svg-icons/faSyncAlt'; // change something, update
+import { faTimes } from '@fortawesome/free-solid-svg-icons/faTimes'; // keypad cancel btn
+import { faTimesCircle } from '@fortawesome/free-solid-svg-icons/faTimesCircle'; // closing btn
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons/faTrashAlt'; // delete btn
+import { faTrophy } from '@fortawesome/free-solid-svg-icons/faTrophy';
+import { faUser } from '@fortawesome/free-solid-svg-icons/faUser';
+// app milestone icons
+import { faAppleAlt } from '@fortawesome/free-solid-svg-icons/faAppleAlt';
+import { faCar } from '@fortawesome/free-solid-svg-icons/faCar';
+import { faCartPlus } from '@fortawesome/free-solid-svg-icons/faCartPlus';
+import { faCircle } from '@fortawesome/free-solid-svg-icons/faCircle';
+import { faCocktail } from '@fortawesome/free-solid-svg-icons/faCocktail';
+import { faCut } from '@fortawesome/free-solid-svg-icons/faCut';
+import { faDragon } from '@fortawesome/free-solid-svg-icons/faDragon';
+import { faDumbbell } from '@fortawesome/free-solid-svg-icons/faDumbbell';
+import { faFire } from '@fortawesome/free-solid-svg-icons/faFire';
+import { faGamepad } from '@fortawesome/free-solid-svg-icons/faGamepad';
+import { faGrinHearts } from '@fortawesome/free-solid-svg-icons/faGrinHearts';
+import { faHamburger } from '@fortawesome/free-solid-svg-icons/faHamburger';
+import { faHeart } from '@fortawesome/free-solid-svg-icons/faHeart';
+import { faIceCream } from '@fortawesome/free-solid-svg-icons/faIceCream';
+import { faMedal } from '@fortawesome/free-solid-svg-icons/faMedal';
+import { faMusic } from '@fortawesome/free-solid-svg-icons/faMusic';
+import { faPaw } from '@fortawesome/free-solid-svg-icons/faPaw';
+import { faRobot } from '@fortawesome/free-solid-svg-icons/faRobot';
+import { faStar } from '@fortawesome/free-solid-svg-icons/faStar';
+import { faStethoscope } from '@fortawesome/free-solid-svg-icons/faStethoscope';
+import { faSun } from '@fortawesome/free-solid-svg-icons/faSun';
+import { faTshirt } from '@fortawesome/free-solid-svg-icons/faTshirt';
+import { faUserTie } from '@fortawesome/free-solid-svg-icons/faUserTie';
+// end app milestone icons
 
 library.add(
-  // app milestone icons
-  faHeart,
-  faStar,
-  faIceCream,
-  faSun,
-  faHamburger,
-  faTshirt,
-  faCut,
-  faCircle,
-  faPaw,
-  faCocktail,
-  faMusic,
-  faRobot,
-  faMedal,
-  faStethoscope,
-  faCar,
-  faDragon,
-  faGrinHearts,
-  faUserTie,
-  faFire,
-  faGamepad,
-  faDumbbell,
-  faAppleAlt,
-  faCartPlus,
-  //end app milestone icons
-  faFlagCheckered,
-  faExclamationCircle,
-  faInfoCircle,
-  faCheckCircle,
-  faCheck,
-  faPaperPlane,
-  faLock,
-  faSave,
-  faKeyboard,
   faAngleDoubleRight,
-  faQuestion,
-  faMobileAlt,
-  faShareAlt,
-  faMinus,
-  faPlus,
-  faTimes,
-  faTimesCircle,
+  faAngleRight,
   faArrowLeft,
+  faChartPie,
+  faCheck,
+  faCheckCircle,
+  faClock,
+  faCogs,
+  faComment,
+  faCrown,
+  faDatabase,
+  faExclamationCircle,
+  faFileAlt,
+  faFlagCheckered,
+  faGem,
+  faHome,
+  faImage,
+  faInfoCircle,
+  faKeyboard,
   faListOl,
   faListUl,
-  faMoneyBillAlt,
-  faTrashAlt,
-  faMinusCircle,
-  faUser,
-  faQuestionCircle,
-  faShoppingCart,
-  faTrophy,
-  faCrown,
-  faPalette,
-  faGem,
-  faComment,
-  faClock,
-  faFileAlt,
-  faDatabase,
-  faCogs,
-  faChartPie,
-  faHome,
-  faAngleRight,
+  faLock,
   faLongArrowAltDown,
-  faImage,
+  faMinus,
+  faMinusCircle,
+  faMobileAlt,
+  faMoneyBillAlt,
+  faPalette,
+  faPaperPlane,
+  faPlus,
+  faQuestion,
+  faQuestionCircle,
+  faSave,
+  faShareAlt,
+  faShoppingCart,
   faSyncAlt,
-  faReply,
+  faTimes,
+  faTimesCircle,
+  faTrashAlt,
+  faTrophy,
+  faUser,
+  // app milestone icons
+  faAppleAlt,
+  faCar,
+  faCartPlus,
+  faCircle,
+  faCocktail,
+  faCut,
+  faDragon,
+  faDumbbell,
+  faFire,
+  faGamepad,
+  faGrinHearts,
+  faHamburger,
+  faHeart,
+  faIceCream,
+  faMedal,
+  faMusic,
+  faPaw,
+  faRobot,
+  faStar,
+  faStethoscope,
+  faSun,
+  faTshirt,
+  faUserTie,
+  //end app milestone icons
 );
+
+/* COMMENTS
+n1: solution with named destructing
+ We weren't able to get tree shaking to work out of the box with Webpack 4. Thankfully, we were able to workaround the issue using babel-plugin-transform-imports with a configuration like this:
+
+ // .babelrc
+ {
+   "plugins": [
+     ["transform-imports", {
+       "@fortawesome/free-solid-svg-icons": {
+         "transform": "@fortawesome/free-solid-svg-icons/${member}",
+         "skipDefaultConversion": true
+       }
+     }]
+   ]
+ }
+ With that, we're still able to import members directly from the package.
+
+ import { faTimes, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+ This works for Pro versions as well.
+https://github.com/FortAwesome/react-fontawesome/issues/70
+*/
