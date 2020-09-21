0767ab3e65dd132a96464884676639c483ff3626
Luis Febro <mr.febro@gmail.com>
2020-09-21 15:48:31 -0400

    página de planos | sistema de pagamento PAGSEGURO | club pro pt19

--- client/src/_main-app/user-interfaces/version/Version.js
+++ client/src/_main-app/user-interfaces/version/Version.js
@@ -4,7 +4,7 @@ import Img from "../../../components/Img";
 
 const isSmall = window.Helper.isSmallScreen();
 
-const latestVersion = "3.5404.4";
+const latestVersion = "3.5405.4";
 // PRÓXIMO:
 // artigo exemplo de pontos.
 const updateDescription =
