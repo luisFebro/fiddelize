a6726231591c811d352090ad617fb2bffc16132f
Luis Febro <mr.febro@gmail.com>
2020-09-29 02:43:34 -0400

    página de planos | sistema de pagamento PAGSEGURO | club pro pt31

--- client/src/_main-app/user-interfaces/version/Version.js
+++ client/src/_main-app/user-interfaces/version/Version.js
@@ -4,7 +4,7 @@ import Img from "../../../components/Img";
 
 const isSmall = window.Helper.isSmallScreen();
 
-const latestVersion = "3.5703.4";
+const latestVersion = "3.5704.4";
 // PRÓXIMO:
 //
 const updateDescription =
--- controllers/pay/globalVar.js
+++ controllers/pay/globalVar.js
@@ -1,9 +1,9 @@
 const { IS_PROD } = require("../../config");
 
-// On production, sandbox request will return "Not found" because the host address will be production's, not dev.
+// IMPORTANT! On production for boleto, sandbox request will return "Not found" because the host address will be production's, not dev.
 // In order to work, we have to set sandboxMode to TRUE and deploy both here and PayArea Compenent.
 exports.globalVar = {
-    sandboxMode: true, //IS_PROD ? false : true,
+    sandboxMode: IS_PROD ? false : true,
     get payUrl() {
         return this.sandboxMode
             ? "https://ws.sandbox.pagseguro.uol.com.br"
