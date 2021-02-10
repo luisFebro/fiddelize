ec197c6da84170efed467913bdcfc221718d2401
Luis Febro <mr.febro@gmail.com>
2021-02-10 14:08:49 -0400

    testing 10

--- client/.lintstagedrc.js
+++ client/.lintstagedrc.js
@@ -4,6 +4,10 @@
 // }
 
 module.exports = {
-    "*.{js,jsx,css}": (filenames) =>
-        filenames.map((filename) => `prettier --write '${filename}'`),
+    "*.{js,jsx,css}": [
+        (filenames) =>
+            filenames.map((filename) => `prettier --write '${filename}'`),
+        (filenames) =>
+            filenames.map((filename) => `eslint --fix '${filename}'`),
+    ],
 };
