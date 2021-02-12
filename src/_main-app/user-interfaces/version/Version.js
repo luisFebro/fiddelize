8e096e136655d67942250d5867b0641855d02a64
Luis Febro <mr.febro@gmail.com>
2021-02-12 01:12:07 -0400

    separação oficial do front e backend do projeto pt 3.5

--- src/_main-app/user-interfaces/version/Version.js
+++ src/_main-app/user-interfaces/version/Version.js
@@ -4,7 +4,7 @@ import Img from "../../../components/Img";
 
 const isSmall = window.Helper.isSmallScreen();
 
-const latestVersion = "3.99.8"; // PRÓXIMO:
+const latestVersion = "3.9902.8"; // PRÓXIMO:
 // app fiddelize pt 8
 // split de pagamento backend pt2
 const updateDescription =
