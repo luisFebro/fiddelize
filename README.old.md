a767876921c10e7eac7a1eda01696fc087d912cc
Luis Febro <mr.febro@gmail.com>
2021-02-11 21:05:46 -0400

    separação oficial do front e backend do projeto pt 1

--- README.old.md
+++
@@ -1,8 +0,0 @@
-## Command CLI
-- install packages:
-`cd backend && npm install && npm run client-install`
-
-- start the project:
-`cd backend && npm run dev`
-
-## Developer: Luis Febro.
--- config.js
+++
@@ -1,7 +0,0 @@
-exports.CLIENT_URL =
-    process.env.NODE_ENV === "production"
-        ? "https://fiddelize.com.br"
-        : "http://localhost:3000";
-
-const ENVIRONMENT = process.env.NODE_ENV || "development";
-exports.IS_PROD = ENVIRONMENT === "production";
