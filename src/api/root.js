4ac909c93f905e1c8f0778e3b1c86f671494ada5
Luis Febro <mr.febro@gmail.com>
2021-11-16 21:55:14 -0400

    staging

--- src/api/root.js
+++ src/api/root.js
@@ -4,7 +4,7 @@ const IS_PROD = process.env.NODE_ENV === "production";
 // In a branch feature, set staging in api/root.js (frontend) when required to test in staging production when devoloping a new feature.
 // IMPORTANT: this should be set to false when merging to master.
 // it is recommended to set to false right after making a deploy
-const IS_STAGING = true;
+const IS_STAGING = false; //
 
 const prodLink = IS_STAGING
     ? "https://fiddelize-test.herokuapp.com/api"
