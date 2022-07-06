c2236bb7ef2ffaab8ae340b50e4c3cadb416d8c7
Luis Febro <mr.febro@gmail.com>
2022-07-06 09:41:04 -0400

    docs

--- docs/marica-docs/vpn/BaseV700/BaseV700/app/src/main/java/com/bsev700/br/BASEV700App.java
+++ docs/marica-docs/vpn/BaseV700/BaseV700/app/src/main/java/com/bsev700/br/BASEV700App.java
@@ -15,20 +15,20 @@ public class BASEV700App extends Application
 	private static final String TAG = BASEV700App.class.getSimpleName();
 	public static final String PREFS_GERAL = "BASEV700GERAL";
 
-	
+
 	private static BASEV700App mApp;
-	
+
 	@Override
 	public void onCreate()
 	{
 		super.onCreate();
 
 		mApp = this;
-			
+
 		// inicia
 		BASEV700Core.init(this);
 	}
-	
+
 	@Override
 	protected void attachBaseContext(Context base) {
 		super.attachBaseContext(base);
@@ -41,7 +41,7 @@ public class BASEV700App extends Application
 		//LocaleHelper.setLocale(this);
 	}
 
-	
+
 	public static BASEV700App getApp() {
 		return mApp;
 	}
